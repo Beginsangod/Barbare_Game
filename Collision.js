d708e3392cb085e79b8666d6c5afebd08c3e39c0
Beginsangod <fernandalouka345@gmail.com>
2026-06-12 12:28:05 +0100

    collision et ajout d'assets

---
+++ Collision.js
@@ -0,0 +1,7 @@
+
+export function check_collision(sprite1, sprite2) {
+    return sprite1.x > sprite2.x + sprite2.width &&
+           sprite1.x + sprite1.width < sprite2.x &&
+           sprite1.y > sprite2.y + sprite2.height &&
+           sprite1.y + sprite1.height < sprite2.y
+}
